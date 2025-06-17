affd9305286313ec714126e0d0e09fb1ff24d928
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-06-17 20:40:31 +0700

    remove playground

--- packages/go.dev/backend/reverse-proxy/gorilla/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/hieudoanm/reverse-proxy
-
-go 1.23.6
-
-require github.com/gorilla/mux v1.8.1 // indirect
--- packages/node.js/server/reverse-proxy/nest/src/health/health.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class Status {
-  @ApiProperty({ name: 'status' })
-  public status: string;
-}
